5c8ab16b0539f8df184fb6d0c05eac2be8c32aa7
Jairo Matos da Rocha <devjairomr@gmail.com>
2022-09-29 11:10:19 -0300

    keyapi

--- ServeStatus/functions.py
+++ ServeStatus/functions.py
@@ -1,11 +1,10 @@
 from functools import wraps
 from flask import request
+from ServeStatus.config import settings
 def is_lapig_user(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if not request.args.get('key'):
-            return 'bad request!', 401
-        if not request.args.get('key') == 'LAPIG2022GEE':
+        if not request.args.get('key') or not request.args.get('key') == settings.KEYAPI:
             return 'bad request!', 401
         return f(*args, **kwargs)
    
